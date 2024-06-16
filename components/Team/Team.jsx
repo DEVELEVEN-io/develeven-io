1517d57cae9f4dadcda1ab994940ecf5d49f1862
Ragib Al Asad <ragibalasad@gmail.com>
2024-06-16 20:29:01 +0600

    added my picture

--- components/Team/Team.jsx
+++ components/Team/Team.jsx
@@ -10,13 +10,13 @@ const Team = () => {
       <div className="row flex flex-row my-4">
 
         <div className="memberCard bg-bglight rounded-t-md rounded-l-md rounded-b-none flex flex-col mx-auto">
-          <Image src={images.portrait_1_md} alt="Logo" width={300} height={300}/>
+          <Image src={images.portrait_abid_md} alt="Logo" width={300} height={300}/>
           <div className="memberGradient absolute inset-0 rounded-t-md rounded-l-md rounded-b-none" />
           <div className="memberInfo flex flex-col mx-auto">
             <span className="memberName mb-1">Abid Al Wassie</span>
             <span className="memberTitle text-highlight my-0">Web Developer</span>
             <div className="memberDesc">
-              <p className="memberText">Abid Al Wassie is the senior developer of DevEleven-io. He is a full stack developer with 4+ years of experience. He is the creator of oneManDev. He has a long history with JavaScript and other technologies.</p>
+              <p className="memberText">Abid Al Wassie is a senior developer of DevEleven-io. He is a full stack developer with 4+ years of experience. He is the creator of oneManDev. He has a long history with JavaScript and other technologies.</p>
               <ul className="memberLinks flex">
                 <li className="text-4xl mx-auto mb-4 mt-8"><a href="https://abidalwassie.me" target="_blank"><FaGlobe /></a></li>
                 <li className="text-4xl mx-auto mb-4 mt-8"><a href="https://github.com/AbidAlWassie" target="_blank"><FaGithub /></a></li>
@@ -28,18 +28,18 @@ const Team = () => {
         </div>
 
         <div className="memberCard bg-bglight rounded-t-md rounded-l-md rounded-b-none flex flex-col mx-auto">
-          <Image src={images.portrait_1_md} alt="Logo" width={300} height={300}/>
+          <Image src={images.portrait_ragib_md} alt="Logo" width={300} height={300}/>
           <div className="memberGradient absolute inset-0 rounded-t-md rounded-l-md rounded-b-none" />
           <div className="memberInfo flex flex-col mx-auto">
-          <span className="memberName mb-1">Abid Al Wassie</span>
+          <span className="memberName mb-1">Ragib Al Asad</span>
             <span className="memberTitle text-highlight my-0">Web Developer</span>
             <div className="memberDesc">
-              <p className="memberText">Abid Al Wassie is the senior developer of DevEleven-io. He is a full stack developer with 4+ years of experience. He is the creator of oneManDev. He has a long history with JavaScript and other technologies.</p>
+              <p className="memberText">Ragib Al Asad is a senior backend developer of DevEleven-io with 4+ years of experience. He is the founder of DevEleven. Holds strong understandings in backend web technology like FastAPI</p>
               <ul className="memberLinks flex">
-                <li className="text-4xl mx-auto mb-4 mt-8"><a href="https://abidalwassie.me" target="_blank"><FaGlobe /></a></li>
-                <li className="text-4xl mx-auto mb-4 mt-8"><a href="https://github.com/AbidAlWassie" target="_blank"><FaGithub /></a></li>
-                <li className="text-4xl mx-auto mb-4 mt-8"><a href="https://youtube.com/@oneManDev" target="_blank"><FaYoutube /></a></li>
-                <li className="text-4xl mx-auto mb-4 mt-8"><a href="https://www.linkedin.com/in/abidalwassie" target="_blank"><FaLinkedin /></a></li>
+                <li className="text-4xl mx-auto mb-4 mt-8"><a href="https://facebook.com/ragibalasad" target="_blank"><FaGlobe /></a></li>
+                <li className="text-4xl mx-auto mb-4 mt-8"><a href="https://github.com/ragibalasad" target="_blank"><FaGithub /></a></li>
+                <li className="text-4xl mx-auto mb-4 mt-8"><a href="https://youtube.com/@ragibalasad" target="_blank"><FaYoutube /></a></li>
+                <li className="text-4xl mx-auto mb-4 mt-8"><a href="https://www.linkedin.com/in/ragibalasad" target="_blank"><FaLinkedin /></a></li>
               </ul>
             </div>
           </div>
--- constants/images.jsx
+++ constants/images.jsx
@@ -1,11 +1,15 @@
 import profile_sm from '../assets/profile_sm.png';
 import portrait_1_md from '../assets/portrait_1_md.png';
+import portrait_abid_md from '../assets/portrait_abid_md.png';
+import portrait_ragib_md from '../assets/portrait_ragib_md.png';
 import logo from '../assets/logo.png';
 
 
 const data = {
   profile_sm,
   portrait_1_md,
+  portrait_abid_md,
+  portrait_ragib_md,
   logo,
 };
 
