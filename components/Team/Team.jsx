8ef4f37ccd9f337580a0da8238088d388d16945f
Abid Al Wassie <abidalwassie@outlook.com>
2024-06-26 17:43:48 +0600

    updated project

--- components/Team/Team.jsx
+++ components/Team/Team.jsx
@@ -40,10 +40,10 @@ const teamMembers = [
     }
   },
   {
-    name: "Abid Al Wassie",
-    image: images.portrait_1_md,
+    name: "Raiyan Takrim",
+    image: images.portrait_raiyan_md,
     title: "Web Developer",
-    description: "Abid Al Wassie is the senior developer of DevEleven-io. He is a full stack developer with 4+ years of experience. He is the creator of oneManDev. He has a long history with JavaScript and other technologies.",
+    description: "Raiyan Takrim  is a frontend developer at DevEleven-io. He posseses experties in HTML, CSS, JS, Python, React etc.",
     links: {
       website: "https://abidalwassie.me",
       github: "https://github.com/AbidAlWassie",
@@ -62,7 +62,7 @@ const Team = () => {
       <div className="memberCards row flex flex-row my-4">
         {teamMembers.map((member, index) => (
           <div key={index} className="memberCard bg-bglight rounded-t-md rounded-l-md rounded-b-none flex flex-col mx-auto relative">
-            <Image src={member.image} alt={member.name} width={300} height={600} className="object-cover rounded-t-md rounded-l-md rounded-b-none" />
+            <Image src={member.image} alt={member.name} width={315} height={386.4} className="object-cover rounded-t-md rounded-l-md rounded-b-none" />
             <div className="memberGradient absolute inset-0 rounded-t-md rounded-l-md rounded-b-none" />
             <div className="memberInfo flex flex-col mx-auto">
               <span className="memberName mb-1">{member.name}</span>
