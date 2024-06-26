8d58c276b85d4b251eb92e155d8af14bcd914da6
Abid Al Wassie <abidalwassie@outlook.com>
2024-06-26 17:11:40 +0600

    updated project

--- constants/images.jsx
+++ constants/images.jsx
@@ -6,23 +6,23 @@ import portrait_rashu_md from '../assets/portrait_rashu_md.png';
 import portrait_raiyan_md from '../assets/portrait_raiyan_md.png';
 import logo from '../assets/logo.png';
 
-import cpp from '../assets/icons/cpp.png';
 import html from '../assets/icons/html.png';
 import css from '../assets/icons/css.png';
 import javascript from '../assets/icons/javascript.png';
 import react from '../assets/icons/react.png';
-import laravel from '../assets/icons/laravel.png';
 import tailwind from '../assets/icons/tailwind.png';
 import figma from '../assets/icons/figma.png';
-import flutter from '../assets/icons/flutter.png';
 import git from '../assets/icons/git.png';
-import graphql from '../assets/icons/graphql.png';
-import node from '../assets/icons/node.png';
 import python from '../assets/icons/python.png';
 import typescript from '../assets/icons/typescript.png';
-import vue from '../assets/icons/vue.png';
 import nextjs from '../assets/icons/nextjs.png';
 import firebase from '../assets/icons/firebase.png';
+// import cpp from '../assets/icons/cpp.png';
+// import flutter from '../assets/icons/flutter.png';
+// import graphql from '../assets/icons/graphql.png';
+// import node from '../assets/icons/node.png';
+// import laravel from '../assets/icons/laravel.png';
+// import vue from '../assets/icons/vue.png';
 
 
 const data = {
