fb24fcdf651e608004c9567a28b5ee741eca8484
Abid Al Wassie <abidalwassie@outlook.com>
2024-06-26 16:24:53 +0600

    updated tech

--- components/Tech/Tech.jsx
+++ components/Tech/Tech.jsx
@@ -1,16 +1,43 @@
-// import { images } from "../../constants";
-// import Image from 'next/image';
-// import { FaLinkedin, FaGithub, FaYoutube, FaGlobe } from 'react-icons/fa';
+import { images } from '../../constants';
+import Image from 'next/image';
+
+const skillsData = [
+  { name: 'HTML5', proficiency: 98, image: images.html, circleClass: 'p-circle1' },
+  { name: 'CSS3', proficiency: 95, image: images.css, circleClass: 'p-circle2' },
+  { name: 'JavaScript', proficiency: 90, image: images.javascript, circleClass: 'p-circle3' },
+  { name: 'React', proficiency: 75, image: images.react, circleClass: 'p-circle4' },
+  { name: 'Next.js', proficiency: 70, image: images.nextjs, circleClass: 'p-circle6' },
+  { name: 'Tailwind', proficiency: 90, image: images.tailwind, circleClass: 'p-circle7' },
+  { name: 'Python', proficiency: 75, image: images.python, circleClass: 'p-circle8' },
+  { name: 'Figma', proficiency: 90, image: images.figma, circleClass: 'p-circle9' },
+  { name: 'Git', proficiency: 100, image: images.git, circleClass: 'p-circle10' },
+  { name: 'Firebase', proficiency: 55, image: images.firebase, circleClass: 'p-circle11' },
+];
+
 const Tech = () => {
   return (
     <section id="tech" className="container flex flex-col justify-between mx-auto mt-14 w-full">
       <h2 className="text-4xl font-bold text-gradient my-2">Technologies we use</h2>
       <p className="my-4">We try to adapt to the latest and the most advanced tech and frameworks on our projects.</p>
 
-      <div className="bg-bglight py-24 h-96">
-
+      <div className="skills-wrap">
+        {skillsData.map((skill, index) => (
+          <div key={index} className="skill">
+            <div className="sk-progress">
+              <svg className={skill.circleClass}>
+                <circle className={skill.circleClass} cx={75} cy={75} r={68} />
+              </svg>
+              <div className="techLogo">
+                <Image loading="eager" src={skill.image} alt={`${skill.name} logo`} />
+              </div>
+              <h2 className="counter">
+                <span data-target={skill.proficiency}>0</span>%
+              </h2>
+              <h1 className="sk-title">{skill.name}</h1>
+            </div>
+          </div>
+        ))}
       </div>
-      
     </section>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -31,3 +31,79 @@ export default function Home() {
     </div>
   );
 }
+
+let skillsPlayed = false;
+let mlPlayed = false;
+
+if (typeof window !== "undefined") {
+window.addEventListener("scroll", () => {
+
+  if (!skillsPlayed) loadSkills();
+
+  if (!mlPlayed) mlCounter();
+
+});
+}
+
+function updateCount(num, maxNum) {
+  let currentNum = +num.innerText;
+
+  if (currentNum < maxNum) {
+    num.innerText = currentNum + 1;
+    setTimeout(() => {
+      updateCount(num, maxNum);
+    }, 12);
+  }
+}
+
+
+function loadSkills() {
+  const skCounters = document.querySelectorAll(".counter span");
+  const progressBars = document.querySelectorAll(".sk-progress svg circle");
+  // const scrollable =  document.documentElement.scrollHeight - window.innerHeight;
+  
+    const scrolled = window.scrollY;
+
+    if (scrolled >= 1200) {
+      
+      for (let i = 0; i < skCounters.length; i++) {
+
+        skillsPlayed = true;
+
+        const cvalue = Number(skCounters[i].dataset.target);
+        let strokeValue = 427 - 427 * (cvalue/100);
+
+        progressBars[i].style.animation = "progress 2s ease-in-out forwards";
+        progressBars[i].style.setProperty("--target", strokeValue);
+
+        setTimeout(() => {
+          updateCount(skCounters[i], cvalue);
+        }, 400);
+      }
+
+      setTimeout(() => {
+        window.removeEventListener("scroll", loadSkills);
+      }, 2000);
+    }
+}
+
+
+function mlCounter() {
+  // let mlSection = document.querySelectorAll(".milestones");
+  let mlCounters = document.querySelectorAll(".number span");
+  
+  const scrolled = window.scrollY;
+  
+  if (scrolled >= 1750) {
+    // console.log(mlCounters.length);
+    
+    for (let i = 0; i < mlCounters.length; i++) {
+      mlPlayed = true;
+
+      let mvalue = Number(mlCounters[i].dataset.target);
+      setTimeout(() => {
+        updateCount(mlCounters[i], mvalue);
+      }, 1000);
+    }
+  }
+}
