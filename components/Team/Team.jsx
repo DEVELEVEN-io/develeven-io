322a5e0229cc32e8b0cc808555bd0c757042621d
Ragib Al Asad <ragibalasad@gmail.com>
2024-07-13 22:59:15 +0600

    Update Member Info for Ragib

--- components/Team/Team.jsx
+++ components/Team/Team.jsx
@@ -20,7 +20,7 @@ const teamMembers = [
     name: "Ragib Al Asad",
     image: images.portrait_ragib_md,
     title: "Web Developer",
-    description: "Founder of DevEleven. Senior Backend Developer at DevEleven-io with terrible frontend skills.",
+    description: "DevEleven Co-Founder. Senior Backend Developer at DevEleven-io with terrible frontend skills.",
     links: {
       website: "https://ragibalasad.me",
       github: "https://github.com/ragibalasad",
